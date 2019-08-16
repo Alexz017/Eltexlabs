a809aa6bd7cad76b966bfc71ffc23bbac6ea2ee7
Alexandr Gross <alexnsk9754gmail.com>
2019-08-16 13:46:57 +0700

    4,5

--- Eltex_summer_school19/4__laba/Char/4_laba.c
+++ Eltex_summer_school19/4__laba/Char/4_laba.c
@@ -2,35 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 #define N 1024
-int main(int argc, char *argv[]){
-  FILE *fp;
-   char ch;
-   
-   if (argc < 1){
-		fprintf (stderr, "Мало аргументов. Используйте <имя файла> <символ который хотите вставить>\n");
-		exit (1);
-  }
-  if((fp=fopen(argv[1],"r"))==NULL) {
-    printf("Невозможно открыть файл.\n");
-    exit(1);
-  }
-  FILE *fp2;
-  fp2=fopen("test1","w");
-	
+/*
+В конце каждой строки вставить заданный символ 
+Параметры командной строки:
+1. Имя входного файла 
+2. Заданный символ
+*/
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+    char ch;
 
-  while((ch=fgetc(fp)) != EOF) {
-	if (ch =='\n')
-	{
-		fputs( argv[2],fp2);
-	 printf("%s",argv[2]);
-	 
- }
-	 printf("%c", ch);
-	fputc( ch,fp2);
-	
-  }
-  
-  fclose(fp2);
-  fclose(fp);
-  return 0;
+    if (argc < 1)
+    {
+        fprintf(stderr, "Мало аргументов. Используйте <имя файла> <символ который хотите вставить>\n");
+        exit(1);
+    }
+    if ((fp = fopen(argv[1], "r")) == NULL)
+    {
+        printf("Невозможно открыть файл.\n");
+        exit(1);
+    }
+    FILE *fp2;
+    fp2 = fopen("test1", "w");
+    printf("Содержание файла %s\n", argv[1]);
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        printf("%c", ch);
+    }
+    rewind(fp);
+    printf("Содержание файла после изменения\n");
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        if (ch == '\n')
+        {
+            fputs(argv[2], fp2);
+            printf("%s", argv[2]);
+        }
+        printf("%c", ch);
+        fputc(ch, fp2);
+    }
+
+    fclose(fp2);
+    fclose(fp);
+    return 0;
 }
--- Eltex_summer_school19/4__laba/String/4_laba.c
+++ Eltex_summer_school19/4__laba/String/4_laba.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define N 1024
+#define N 1500
+/*
+Оставить только строки, начинающиеся с латинских букв 
+	Параметры командной строки:
+	1. Имя входного файла
+	2. Количество обрабатываемых строк 
+*/
+
 int main(int argc, char *argv[]){
 	 FILE *fp;
    char buffer[N];
-if (argc < 3 ){
+if (argc < 2 ){
 		fprintf (stderr, "Мало аргументов. Используйте <имя файла> <количество строк>\n");
 		exit (1);
   }
@@ -17,13 +24,24 @@ if (argc < 3 ){
   int i=0;
   FILE *fp2;
   fp2=fopen("test","w");
-  while((!feof(fp))&(i<d)) {
-	  
+  printf("Содержание файла %s",argv[1]);
+  while (!feof(fp)) {
 	 fgets(buffer, N, fp);
-	 fputs(buffer,fp2);
 	 printf("%s", buffer);
-	 i++;
-    
+	
+}
+printf("\n");    
+rewind(fp);
+printf("количество строк: %s \n",argv[2]);
+printf("критерий отбора: строки, должны начинаться с латинских букв:\n");
+  while((!feof(fp))&(i<d)) {
+	  int s=0;
+	 fgets(buffer, N, fp);
+	 s=buffer[0];
+	 if ((s>64)&(s<123)){
+	fputs(buffer,fp2);
+	 printf("%s", buffer);
+}    
 }
 
 
